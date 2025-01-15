b223ddb35af40a337d3ed1b6966f97122e1ed866
BassmaKhaled <112968009+bassma-khaled14@users.noreply.github.com>
2025-01-15 17:53:39 +0200

    lastupd

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def calculator():
     return render_template('calc.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True),
+    app.run(debug=True,port=5000),
